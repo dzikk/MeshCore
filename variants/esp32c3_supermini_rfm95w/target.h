5b8193050c97978e85ce04a178408b3156504260
dzikk <dzikk@op.pl>
2026-08-08 01:54:49 +0200

    Update includes and add radio configuration functions

--- variants/esp32c3_supermini_rfm95w/target.h
+++ variants/esp32c3_supermini_rfm95w/target.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #define RADIOLIB_STATIC_ONLY 1
-#include <RadioLib.h>
-#include <helpers/radiolib/RadioLibWrappers.h>
-#include <XiaoC3Board.h>
-#include <helpers/radiolib/CustomSX1262Wrapper.h>
-#include <helpers/AutoDiscoverRTCClock.h>
+
+#include <helpers/radiolib/CustomSX1276.h>
+#include <helpers/radiolib/RadioLibWrapper.h>
+#include <helpers/esp32/ESP32RTCClock.h>
+#include <helpers/esp32/AutoDiscoverRTCClock.h>
 #include <helpers/sensors/EnvironmentSensorManager.h>
+#include <helpers/BaseChatMesh.h>
+
+#include "XiaoC3Board.h"
 
 extern XiaoC3Board board;
 extern WRAPPER_CLASS radio_driver;
@@ -14,4 +17,7 @@ extern AutoDiscoverRTCClock rtc_clock;
 extern EnvironmentSensorManager sensors;
 
 bool radio_init();
+uint32_t radio_get_rng_seed();
+void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr);
+void radio_set_tx_power(int8_t dbm);
 mesh::LocalIdentity radio_new_identity();
