b971cf56265c6ff81834bff41b550bfe75da941e
dzikk <dzikk@op.pl>
2026-08-08 02:09:23 +0200

    Update radio header includes and identity function

--- variants/esp32c3_supermini_rfm95w/target.h
+++ variants/esp32c3_supermini_rfm95w/target.h
@@ -2,8 +2,7 @@
 
 #define RADIOLIB_STATIC_ONLY 1
 
-#include <helpers/radiolib/CustomSX1276.h>
-#include <helpers/radiolib/RadioLibWrapper.h>
+#include <helpers/radiolib/CustomSX1276Wrapper.h>
 #include <helpers/esp32/ESP32RTCClock.h>
 #include <helpers/esp32/AutoDiscoverRTCClock.h>
 #include <helpers/sensors/EnvironmentSensorManager.h>
