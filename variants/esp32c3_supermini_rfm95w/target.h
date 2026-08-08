7f434ef34a918a4a654dd5b428540a99b7065d31
dzikk <dzikk@op.pl>
2026-08-08 02:33:57 +0200

    Refactor target.h by cleaning up includes
    Removed unused includes and updated the radio identity function.

--- variants/esp32c3_supermini_rfm95w/target.h
+++ variants/esp32c3_supermini_rfm95w/target.h
@@ -2,13 +2,12 @@
 
 #define RADIOLIB_STATIC_ONLY 1
 
+#include <RadioLib.h>
+#include <helpers/radiolib/RadioLibWrappers.h>
 #include <helpers/radiolib/CustomSX1276Wrapper.h>
-#include <helpers/ESP32RTCClock.h>
 #include <helpers/AutoDiscoverRTCClock.h>
 #include <helpers/sensors/EnvironmentSensorManager.h>
-#include <helpers/BaseChatMesh.h>
-
-#include "XiaoC3Board.h"
+#include <XiaoC3Board.h>
 
 extern XiaoC3Board board;
 extern WRAPPER_CLASS radio_driver;
@@ -16,7 +15,4 @@ extern AutoDiscoverRTCClock rtc_clock;
 extern EnvironmentSensorManager sensors;
 
 bool radio_init();
-uint32_t radio_get_rng_seed();
-void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr);
-void radio_set_tx_power(int8_t dbm);
 mesh::LocalIdentity radio_new_identity();
