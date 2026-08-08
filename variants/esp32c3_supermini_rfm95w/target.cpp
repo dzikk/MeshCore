f1bc656496c64afeda6716b8227df68d9d2c000d
dzikk <dzikk@op.pl>
2026-08-08 02:21:50 +0200

    Enhance radio functionality with new methods
    Added functions for RNG seed retrieval and radio parameter settings.

--- variants/esp32c3_supermini_rfm95w/target.cpp
+++ variants/esp32c3_supermini_rfm95w/target.cpp
@@ -19,19 +19,28 @@ EnvironmentSensorManager sensors;
 
 bool radio_init() {
   fallback_clock.begin();
-
-#if defined(PIN_BOARD_SDA) && defined(PIN_BOARD_SCL)
-  Wire.begin(PIN_BOARD_SDA, PIN_BOARD_SCL);
   rtc_clock.begin(Wire);
-#else
-  rtc_clock.begin(Wire);
-#endif
 
 #if defined(P_LORA_SCLK)
   return radio.std_init(&spi);
 #else
   return radio.std_init();
 #endif
+}
+
+uint32_t radio_get_rng_seed() {
+  return radio.random(0x7FFFFFFF);
+}
+
+void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr) {
+  radio.setFrequency(freq);
+  radio.setSpreadingFactor(sf);
+  radio.setBandwidth(bw);
+  radio.setCodingRate(cr);
+}
+
+void radio_set_tx_power(int8_t dbm) {
+  radio.setOutputPower(dbm);
 }
 
 mesh::LocalIdentity radio_new_identity() {
