15779f16dc762fe38516dd95679a119791233691
dzikk <dzikk@op.pl>
2026-08-13 08:47:57 +0200

    Add SX127x register debug

--- variants/esp32c3_supermini_rfm95w/target.cpp
+++ variants/esp32c3_supermini_rfm95w/target.cpp
@@ -23,6 +23,19 @@ AutoDiscoverRTCClock rtc_clock(fallback_clock);
   EnvironmentSensorManager sensors;
 #endif
 
+static uint8_t sx127x_read_reg(uint8_t addr) {
+  digitalWrite(P_LORA_NSS, LOW);
+  delayMicroseconds(5);
+
+  spi.transfer(addr & 0x7F);
+  uint8_t val = spi.transfer(0x00);
+
+  delayMicroseconds(5);
+  digitalWrite(P_LORA_NSS, HIGH);
+
+  return val;
+}
+
 bool radio_init() {
   Serial.begin(115200);
   delay(2000);
@@ -46,6 +59,24 @@ bool radio_init() {
 #if defined(P_LORA_SCLK)
   Serial.println("Starting SPI...");
   spi.begin(P_LORA_SCLK, P_LORA_MISO, P_LORA_MOSI);
+
+  pinMode(P_LORA_NSS, OUTPUT);
+digitalWrite(P_LORA_NSS, HIGH);
+
+pinMode(P_LORA_RESET, OUTPUT);
+
+digitalWrite(P_LORA_RESET, LOW);
+delay(20);
+digitalWrite(P_LORA_RESET, HIGH);
+delay(100);
+
+Serial.println("Reading SX127x RegVersion...");
+uint8_t ver = sx127x_read_reg(0x42);
+
+Serial.print("SX127x RegVersion = 0x");
+Serial.println(ver, HEX);
+`
+  
   Serial.println("Calling radio.std_init(&spi)...");
   bool ok = radio.std_init(&spi);
 #else
