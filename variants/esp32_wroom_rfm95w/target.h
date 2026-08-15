395678cf81c961b3b32d762b8636b287fa5aa2c9
dzikk <dzikk@op.pl>
2026-08-15 23:46:32 +0200

    Add target.h for ESP32 WROOM RFM95W variant

---
+++ variants/esp32_wroom_rfm95w/target.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define RADIOLIB_STATIC_ONLY 1
+
+#include <RadioLib.h>
+#include <helpers/radiolib/RadioLibWrappers.h>
+#include <helpers/radiolib/CustomSX1276Wrapper.h>
+#include <helpers/AutoDiscoverRTCClock.h>
+#include <helpers/sensors/EnvironmentSensorManager.h>
+#include <XiaoC3Board.h>
+
+extern XiaoC3Board board;
+extern WRAPPER_CLASS radio_driver;
+extern AutoDiscoverRTCClock rtc_clock;
+extern EnvironmentSensorManager sensors;
+
+bool radio_init();
+mesh::LocalIdentity radio_new_identity();
